befa6c83bd95d52ccd2005163959f562419e343c
DaniPix <domnica.dan@gmail.com>
2016-08-23 18:08:08 +0300

    updates

--- app/src/main/java/dani2pix/ro/foursquareapp/listeners/LocationListener.java
+++ app/src/main/java/dani2pix/ro/foursquareapp/listeners/LocationListener.java
@@ -1,17 +1,12 @@
 package dani2pix.ro.foursquareapp.listeners;
 
-import android.Manifest;
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
 
 import java.lang.ref.WeakReference;
 
